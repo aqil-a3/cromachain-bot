eac3300dd60405cc505e198f899a298de98841b7
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-11-04 14:10:39 +0700

    Update Bot

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@
 
 // startServer();
 
+// Use "type: module" in package.json to use ES modules
 import express from 'express';
 const app = express();
+const port = 3000;
  
 // Define your routes
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
 });
  
-// Export the Express app
-export default app;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
