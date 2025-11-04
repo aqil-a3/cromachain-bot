71ef874626ec3fa176e473d9c907c006a6c623c1
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-11-04 14:36:11 +0700

    Update Bot

--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,12 @@ import { bot } from "./bot";
 import { env } from "./config/env";
 
 const app = express();
-
-// enable JSON body
 app.use(express.json());
 
-// webhook endpoint
 const WEBHOOK_PATH = `/bot${env.BOT_TOKEN}`;
 app.use(WEBHOOK_PATH, bot.webhookCallback(WEBHOOK_PATH));
 
-// base route
-app.get("/", (_, res) => {
-  res.json({ message: "Croma Bot is running on Vercel 🚀" });
-});
-
-// optional: simple health check
-app.get("/health", (_, res) => res.send("OK"));
+app.get("/", (_req, res) => res.json({ message: "Croma Bot is running on Vercel 🚀" }));
+app.get("/health", (_req, res) => res.send("OK"));
 
 export default app;
