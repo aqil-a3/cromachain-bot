31017ed9b7060c5c044b09c2002d448591d1fa8b
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-11-04 14:42:15 +0700

    UPdate Bot

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
-import express from 'express'
+import express from "express";
+import { bot } from "./bot";
+import { env } from "./config/env";
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('Hello Express!')
-})
+const WEBHOOK_PATH = `/bot${env.BOT_TOKEN}`;
+app.use(WEBHOOK_PATH, bot.webhookCallback(WEBHOOK_PATH));
 
-app.get('/api/users/:id', (_req, res) => {
-  res.json({ id: _req.params.id })
-})
+app.get("/", (_req, res) => res.json({ message: "Croma Bot is running on Vercel 🚀" }));
+app.get("/health", (_req, res) => res.send("OK"));
 
-app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
-  res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
-})
-
-export default app
+export default app;
