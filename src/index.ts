4d639d529fdcdbe8b15e98a430788a602157c32b
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-11-04 14:12:23 +0700

    Update Bot

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-// import { startServer } from "./server";
+import { startServer } from "./server";
 
-// startServer();
-
-// Use "type: module" in package.json to use ES modules
-import express from 'express';
-const app = express();
-const port = 3000;
- 
-// Define your routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
-});
- 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+startServer();
